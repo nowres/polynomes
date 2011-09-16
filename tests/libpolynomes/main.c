b3b7af87d2e7e0c78987446e1b591a52c3972a99
Nowres RAFID <nowres.rafed@gmail.com>
2011-09-16 10:25:55 +0000

    adding comments to sample program

--- tests/libpolynomes/main.c
+++ tests/libpolynomes/main.c
@@ -4,13 +4,13 @@
 
 int main()
 {
-    double poly_vals [] = {2,5,1};
+    double poly_vals [] = {2,5,1}; /** P(x) = 2x^2 + 5x + 1 **/
     double val;
     polynome_t *poly;
     poly = create_polynome ();
     set_polynome (poly , poly_vals , 2 );
-    polynome_value (poly , 1 , &val);
+    polynome_value (poly , 1 , &val); /** calculating P(1) / val = P(1) **/
 
-    printf("P(2) = %lf",val);
+    printf("P(1) = %lf",val);
     return 0;
 }
