c13c099b3528f7ec1c824a7f7fbaf007511ad105
Nowres RAFID <nowres.rafed@gmail.com>
2011-09-16 10:22:02 +0000

    adding libpolynomes small test program

---
+++ tests/libpolynomes/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <libpolynomes.h>
+
+int main()
+{
+    double poly_vals [] = {2,5,1};
+    double val;
+    polynome_t *poly;
+    poly = create_polynome ();
+    set_polynome (poly , poly_vals , 2 );
+    polynome_value (poly , 1 , &val);
+
+    printf("P(2) = %lf",val);
+    return 0;
+}
